4056565de63f3ff5f5a82419e8b49ceb106ba3f8
Shiva Bhusal <srollins175@gmail.com>
2026-03-11 17:07:07 +0545

    Toast

---
+++ eCommerce/utils/toast.tsx
@@ -0,0 +1,14 @@
+import Toast from "react-native-toast-message";
+
+export const showToast = (
+  type: "success" | "error",
+  text1: string,
+  text2?: string
+) => {
+  Toast.show({
+    type,
+    text1,
+    text2,
+    position: "top",
+  });
+};
