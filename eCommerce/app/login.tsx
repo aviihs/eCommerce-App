0f205548f6d8c80c2928240d2eb3474be7014eae
Shiva Bhusal <srollins175@gmail.com>
2026-03-12 11:44:36 +0545

    Login Sign up KeyboardSafe Area

--- eCommerce/app/login.tsx
+++ eCommerce/app/login.tsx
@@ -1,4 +1,11 @@
-import { ScrollView, Text, TextInput, Pressable, Image } from "react-native";
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  Pressable,
+  Image,
+  KeyboardAvoidingView,
+} from "react-native";
 import { useRouter } from "expo-router";
 import { useState } from "react";
 import { useAuth } from "../hooks/useAuth";
@@ -16,56 +23,61 @@ export default function Login() {
     if (success) {
       setTimeout(() => {
         router.replace("/(tabs)");
-      }, 500);
+      }, 300);
     }
   };
 
   return (
-    <ScrollView
-      className="bg-white px-6"
-      contentContainerStyle={{
-        flexGrow: 1,
-        justifyContent: "center",
-        paddingBottom: 16,
-      }}
-    >
-      <Image
-        source={require("../assets/images/logo.png")}
-        className="w-72 h-72 mb-10 ml-8"
-        resizeMode="contain"
-      />
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="height">
+      <ScrollView
+        className="bg-white px-6"
+        contentContainerStyle={{
+          flexGrow: 1,
+          justifyContent: "center",
+          paddingBottom: 100,
+        }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <Image
+          source={require("../assets/images/logo.png")}
+          className="w-72 h-72 mb-10 ml-8"
+          resizeMode="contain"
+        />
 
-      <Text className="text-3xl font-bold mb-10 text-center">Login</Text>
+        <Text className="text-3xl font-bold mb-10 text-center">Login</Text>
 
-      <TextInput
-        placeholder="Email Address"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-        className="border border-gray-300 rounded-xl px-5 py-4 mb-4"
-      />
+        <TextInput
+          placeholder="Email Address"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          className="border border-gray-300 rounded-xl px-5 py-3 mb-4"
+        />
 
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-        className="border border-gray-300 rounded-xl px-5 py-4 mb-6"
-      />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+          className="border border-gray-300 rounded-xl px-5 py-3 mb-4"
+        />
 
-      <Pressable className="bg-[#2563eb] py-4 rounded-xl" onPress={handleLogin}>
-        <Text className="text-white text-center text-lg font-semibold">
-          Login
-        </Text>
-      </Pressable>
+        <Pressable
+          className="bg-[#2563eb] py-3 rounded-xl"
+          onPress={handleLogin}
+        >
+          <Text className="text-white text-center text-lg font-semibold">
+            Login
+          </Text>
+        </Pressable>
 
-      <Pressable className="mt-6" onPress={() => router.push("/signup")}>
-        <Text className="text-center text-gray-500">
-          Don&apos;t have account?
-          <Text className="text-[#2563eb]"> Sign Up</Text>
-        </Text>
-      </Pressable>
-    </ScrollView>
+        <Pressable className="mt-6" onPress={() => router.push("/signup")}>
+          <Text className="text-center text-gray-500">
+            Don&apos;t have account?
+            <Text className="text-[#2563eb]"> Sign Up</Text>
+          </Text>
+        </Pressable>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
-
--- eCommerce/app/signup.tsx
+++ eCommerce/app/signup.tsx
@@ -5,6 +5,7 @@ import {
   TextInput,
   Pressable,
   Image,
+  KeyboardAvoidingView,
 } from "react-native";
 import { useRouter } from "expo-router";
 import { useState } from "react";
@@ -20,80 +21,76 @@ export default function Signup() {
   const [password, setPassword] = useState("");
 
   const handleSignup = () => {
-    signup(
-      { name, email, phone, password },
-      router
-    );
+    signup({ name, email, phone, password }, router);
   };
 
   return (
-    <ScrollView
-      className="bg-white px-6"
-      contentContainerStyle={{ flexGrow: 1, justifyContent: "center", paddingBottom: 16  }}
-    >
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="height">
+      <ScrollView
+        className="bg-white px-6"
+        contentContainerStyle={{
+          flexGrow: 1,
+          justifyContent: "center",
+          paddingBottom: 100,
+        }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View>
+          <Image
+            source={require("../assets/images/logo.png")}
+            className="w-60 h-60 mb-10 self-center"
+            resizeMode="contain"
+          />
 
-      <View>
-        <Image
-          source={require("../assets/images/logo.png")}
-          className="w-60 h-60 mb-10 self-center"
-          resizeMode="contain"
-        />
+          <Text className="text-3xl font-bold mb-10 text-center">Sign Up</Text>
 
-        <Text className="text-3xl font-bold mb-10 text-center">
-          Sign Up
-        </Text>
+          <TextInput
+            placeholder="Full Name"
+            value={name}
+            onChangeText={setName}
+            className="border border-gray-300 rounded-xl px-5 py-3 mb-3"
+          />
 
-        <TextInput
-          placeholder="Full Name"
-          value={name}
-          onChangeText={setName}
-          className="border border-gray-300 rounded-xl px-5 py-3 mb-3"
-        />
+          <TextInput
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            className="border border-gray-300 rounded-xl px-5 py-3 mb-3"
+          />
 
-        <TextInput
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          className="border border-gray-300 rounded-xl px-5 py-3 mb-3"
-        />
+          <TextInput
+            placeholder="Phone Number"
+            value={phone}
+            onChangeText={setPhone}
+            keyboardType="phone-pad"
+            className="border border-gray-300 rounded-xl px-5 py-3 mb-3"
+          />
 
-        <TextInput
-          placeholder="Phone Number"
-          value={phone}
-          onChangeText={setPhone}
-          keyboardType="phone-pad"
-          className="border border-gray-300 rounded-xl px-5 py-3 mb-3"
-        />
+          <TextInput
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            className="border border-gray-300 rounded-xl px-5 py-3 mb-4"
+          />
 
-        <TextInput
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-          className="border border-gray-300 rounded-xl px-5 py-3 mb-4"
-        />
+          <Pressable
+            className="bg-blue-600 py-3 rounded-xl"
+            onPress={handleSignup}
+          >
+            <Text className="text-white text-center text-lg font-semibold">
+              Create Account
+            </Text>
+          </Pressable>
 
-        <Pressable
-          className="bg-blue-600 py-3 rounded-xl"
-          onPress={handleSignup}
-        >
-          <Text className="text-white text-center text-lg font-semibold">
-            Create Account
-          </Text>
-        </Pressable>
-
-        <Pressable
-          className="mt-6"
-          onPress={() => router.push("/login")}
-        >
-          <Text className="text-center text-gray-500">
-            Already have account?{" "}
-            <Text className="text-blue-600">Login</Text>
-          </Text>
-        </Pressable>
-      </View>
-
-    </ScrollView>
+          <Pressable className="mt-6" onPress={() => router.push("/login")}>
+            <Text className="text-center text-gray-500">
+              Already have account? <Text className="text-blue-600">Login</Text>
+            </Text>
+          </Pressable>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
